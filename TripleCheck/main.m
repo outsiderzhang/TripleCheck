10900a4f3e239ac12c4d54d6c01e6f87bc4d6072
Zhang <outsider.zhangr@gmail.com>
2017-12-21 17:22:49 +0800

    init project

---
+++ TripleCheck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TripleCheck
+//
+//  Created by sanjia on 2017/12/21.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
